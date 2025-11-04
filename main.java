ca85a12542c971126b8c156230a23056e05292eb
Navya <abc@gamil.com>
2025-11-04 19:21:21 +0530

    updated

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Continuous Integration Test - Jenkins Build Successful!");
+        System.out.println("Jenkins automatically detected my update!");
+
     }
 }
